df9d9d5313818c7de5410039b0dc95323506a196
Caio Plantier Costa <caiolucario20@gmail.com>
2025-10-01 13:52:45 -0300

    Mudanças no layout de login e cadastro para suportar outras resoluções

--- login/login.php
+++ login/login.php
@@ -1,56 +1,48 @@
 <!DOCTYPE html>
-<html lang="pt=br">
+<html lang="pt-br">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Academia PoloFIT</title>
-    <link rel="shortcut icon" href="../imagens/favicon.ico" type="image/x-icon">
-    <link rel="stylesheet" href="loginstyle.css">
+    <title>Login - PoloFIT</title>
+    <link rel="stylesheet" href="cadastrostyle.css"> 
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/7.0.1/css/all.min.css">
+    <link rel="shortcut icon" href="../imagens/favicon.ico" type="image/x-icon">
 </head>
 <body>
-    <?php
-        
-        if (isset($_GET['erro'])) {
+    <main class="container-login">
+
+        <section class="container-formulario-login">
+            <img src="../imagens/LOGOPOLOFIT.png" alt="Logo PoloFIT">
             
-            echo '<p style="color: red; text-align: center;">E-mail ou senha inválidos. Tente novamente.</p>';
-        }
-        ?>
+            <?php
+            // Exibe mensagem de erro, se houver
+            if (isset($_GET['erro'])) {
+                echo '<p class="mensagem-erro">E-mail ou senha inválidos.</p>';
+            }
+            ?>
 
-        <div class="titulo">
-            <label for=""><img src="../imagens/logo polofit.png" alt="Logo 2 da PoloFIT"></label>
-        </div>   
-
-    
-    <form action="../phpbd/verificar_login.php" method="post">
-        
- 
-        <div class="dados">
-
-                <div class="logologin">
-                    <img src="../imagens/LOGOPOLOFIT.png" alt="">
+            <form action="../phpbd/verificar_login.php" method="POST">
+                
+                <label for="email">E-mail:</label>
+                <div class="input-container">
+                    <input type="email" id="email" name="email" placeholder="Informe seu e-mail" required>
+                    <i class="fa-solid fa-envelope"></i>
                 </div>
                 
-                <div class="dadosdados">
-                    <label for="email">E-mail</label>
-                    <br>
-                    <div class="input-container">
-                        <input type="email" name="email" id="" placeholder="Informe seu e-mail" required><i class="fa-solid fa-envelope"></i>
-                    </div>
-                    <br>
-                    <label for="senha">Senha</label>
-                    <br>
-                    <div class="input-container">
-                        <input type="password" name="senha" id="senha" placeholder="Digite sua senha" required pattern=".{4,16}" title="Sua senha deve ter entre 4 e 16 caracteres."><i class="fa-solid fa-key"></i>
-                    </div>
-                    <br>
-                    <button type="submit">Logar</button>
-                    <a href="esqueceu_senha.php">Esqueceu sua senha?</a>
+                <label for="senha">Senha:</label>
+                <div class="input-container">
+                    <input type="password" id="senha" name="senha" placeholder="Digite sua senha" required>
+                    <i class="fa-solid fa-key"></i>
                 </div>
-           
-        </div>
- 
-    </form>
- 
+
+                
+                    <button type="submit">Logar</button>
+
+                <a href="esqueceu_senha.php">Esqueceu sua senha?</a>
+                <a href="../cadastrar.html">Não tem conta? Cadastre-se!</a>
+            </form>
+        </section>
+
+    </main>
 </body>
 </html>
